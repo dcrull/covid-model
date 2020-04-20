a81954e1fc9f01c5c8b754d9b6acd640b7909766
daynan@gmail.com <daynan@gmail.com>
2020-04-20 10:46:14 -0400

    add forecast plot

--- main.py
+++ main.py
@@ -16,19 +16,18 @@ from transformers.power_transformer import PowerT, LogT
 from transformers.ts_models import Naive, FBProph
 from transformers.clean import DropNA
 
-PREP_STEPS = [
-    ('prepnyt', PrepNYT()),
-    ('add_pop', CensusEnrich(query='?get=POP', year='2018', group='pep/charagegroups', api_url='https://api.census.gov/data')),
-    # ('pop_scaler', TargetScaler(target_cols=['cases','deaths'], divisor_col='POP', multiplier=100000.0))
-]
+# some common pipeline steps
+PREP_DICT = {'prepnyt': PrepNYT(),
+              'add_pop': CensusEnrich(query='?get=POP', year='2018', group='pep/charagegroups', api_url='https://api.census.gov/data'),
+              'per_capita': TargetScaler(target_cols=['cases','deaths'], divisor_col='POP', multiplier=100000.0),
+              }
 
-TS_TRANSFORM_STEPS = [
-    ('logtrans', LogT(func='log1p')),
-    ('first_diff', Diff()),
-    ('dropna', DropNA()),
-    # ('naive', Naive(method=np.mean, kwargs={'axis':1})),
-    ('prophet', FBProph()),
-]
+TS_DICT = {'logtrans': LogT(func='log1p'),
+                      'first_diff': Diff(),
+                      'dropna': DropNA(),
+                      'naive': Naive(method=np.mean, kwargs={'axis':1}),
+                      'prophet': FBProph(),
+                      }
 
 class COVPredict:
     def __init__(self,
@@ -36,15 +35,17 @@ class COVPredict:
                  target,
                  nyt_county_url=NYT_COUNTY_URL,
                  nyt_state_url=NYT_STATE_URL,
-                 prep_steps=PREP_STEPS,
-                 ts_transform_steps=TS_TRANSFORM_STEPS,
+                 prep_dict = PREP_DICT,
+                 prep_steps= ['prepnyt','add_pop','per_capita'],
+                 ts_dict = TS_DICT,
+                 ts_steps=['dropna','prophet'],
                  ):
         self.n_forecast = n_forecast
         self.target = target
         self.nyt_state_url = nyt_state_url
         self.nyt_county_url = nyt_county_url
-        self.prep_steps = prep_steps
-        self.ts_transform_steps = ts_transform_steps
+        self.prep_pipe = Pipeline([(k,prep_dict[k]) for k in prep_steps])
+        self.ts_pipe = Pipeline([(k,ts_dict[k]) for k in ts_steps])
 
     @staticmethod
     def expandingsplit(seq, k):
@@ -60,7 +61,7 @@ class COVPredict:
 
     def load_and_prep(self, urlpath):
         data = self.load_nyt(urlpath)
-        return Pipeline(self.prep_steps).fit_transform(data)
+        return self.prep_pipe.fit_transform(data)
 
     def create_ts_feature(self, data, target, diff=True):
         data = data.pivot(index='geoid', columns='date', values=target).fillna(0)
@@ -78,7 +79,7 @@ class COVPredict:
         data = self.load_and_prep(urlpath)
         data = self.create_ts_feature(data, self.target)
         col_chunks = self.expandingsplit(data.columns, k)
-        final_pipe = Pipeline(self.ts_transform_steps)
+        final_pipe = self.ts_pipe
         return {f'fold_{ct}': self.run_inference(data, final_pipe, cols) for ct, cols in enumerate(col_chunks)}
 
     def cv_plot_ts(self, results, idx):
@@ -104,7 +105,7 @@ class COVPredict:
     def final_test(self, urlpath):
         data = self.load_and_prep(urlpath)
         data = self.create_ts_feature(data, self.target)
-        self.final_pipe = Pipeline(self.ts_transform_steps)
+        self.final_pipe = self.ts_pipe
         self.final_X, self.final_y, self.final_yhat = self.run_inference(data, self.final_pipe, data.columns)
 
     def final_plots_and_error(self, idx, err_func):
@@ -124,11 +125,25 @@ class COVPredict:
     def get_forecast(self, urlpath):
         data = self.load_and_prep(urlpath)
         X = self.create_ts_feature(data, self.target)
-        pipe = Pipeline(self.ts_transform_steps)
-        yhat = pipe.fit(X).predict(X)
+        forecast_cols = pd.date_range(start=X.columns[-1] + datetime.timedelta(days=1), periods=self.n_forecast, freq='D')
+        pipe = self.ts_pipe
+        # empty df to set forecast dims
+        y = pd.DataFrame(index=X.index, columns=forecast_cols)
+        yhat = pipe.fit(X, y).predict(X)
         yhat = pipe[:-1].inverse_transform(yhat)
-        yhat.columns = pd.date_range(start=X.columns[-1] + datetime.timedelta(days=1), periods=self.n_forecast, freq='D')
+        yhat.columns = forecast_cols
         return X, yhat
+
+    def plot_forecast(self, X, yhat, idx):
+        plot_ts(X, idx=idx, c='steelblue', label='actual')
+        plot_ts(yhat, idx=idx, c='indianred', label='forecast')
+        label_suffix = 'mean across obs'
+        if isinstance(idx, str): label_suffix = idx
+        scale = 'count'
+        if 'per_capita' in self.prep_pipe.named_steps.keys(): scale = f"per {self.prep_pipe.get_params()['per_capita__multiplier']:0f} people"
+        plt.title(f'actual {self.target} + {self.n_forecast} day forecast for {label_suffix} ({scale})')
+        plt.legend()
+        plt.show()
 
     def save_obj(self, opath):
         with open(opath, 'wb') as f:
