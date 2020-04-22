3931cd805dfb1333397409c7d338a9ef3c352e07
daynan@gmail.com <daynan@gmail.com>
2020-04-22 00:42:28 -0400

    add scaling

--- main.py
+++ main.py
@@ -19,35 +19,42 @@ from transformers.clean import DropNA
 
 # some common pipeline steps
 PREP_DICT = {'prepnyt': PrepNYT(),
-             'add_geo': CensusShapes(),
-             'add_pop': CensusEnrich(query='?get=POP', year='2018', group='pep/charagegroups', api_url='https://api.census.gov/data'),
-             'per_capita': TargetScaler(target_cols=['cases','deaths'], divisor_col='POP', multiplier=100000.0),
-             'per_capita_km': TargetScaler(target_cols=['cases','deaths'], divisor_col='ALAND', multiplier=1000.0),
-              }
+             }
+
+SPATIAL_DICT = {'add_geo': CensusShapes(),
+               'add_pop': CensusEnrich(query='?get=POP', year='2018', group='pep/charagegroups', api_url='https://api.census.gov/data'),
+               }
+
+SCALER_DICT = {'per_1000': ('POP', 1000.0),
+               'per_km': ('ALAND', 1000.0)
+               }
 
 TS_DICT = {'logtrans': LogT(func='log1p'),
-                      'first_diff': Diff(),
-                      'dropna': DropNA(),
-                      'naive': Naive(method=np.mean, kwargs={'axis':1}),
-                      'prophet': FBProph(),
-                      }
+           'first_diff': Diff(),
+           'dropna': DropNA(),
+           'naive': Naive(method=np.mean, kwargs={'axis':1}),
+           'prophet': FBProph(),
+           }
 
 class COVPredict:
     def __init__(self,
                  n_forecast,
-                 target,
                  nyt_county_url=NYT_COUNTY_URL,
                  nyt_state_url=NYT_STATE_URL,
+                 scaler_dict=SCALER_DICT,
                  prep_dict = PREP_DICT,
-                 prep_steps= ['prepnyt','add_geo','add_pop','per_capita','per_capita_km'],
+                 prep_steps= ['prepnyt'],
+                 spatial_dict=SPATIAL_DICT,
+                 spatial_steps=['add_geo', 'add_pop'],
                  ts_dict = TS_DICT,
                  ts_steps=['dropna','prophet'],
                  ):
         self.n_forecast = n_forecast
-        self.target = target
         self.nyt_state_url = nyt_state_url
         self.nyt_county_url = nyt_county_url
+        self.scaler_dict = scaler_dict
         self.prep_pipe = Pipeline([(k,prep_dict[k]) for k in prep_steps])
+        self.spatial_pipe = Pipeline([(k, spatial_dict[k]) for k in spatial_steps])
         self.ts_pipe = Pipeline([(k,ts_dict[k]) for k in ts_steps])
 
     @staticmethod
@@ -55,25 +62,23 @@ class COVPredict:
         q, r = divmod(len(seq), k)
         return (seq[0:(i + 1) * q + min(i + 1, r)] for i in range(k))
 
-    @staticmethod
-    def load_nyt(url):
-        return pd.read_csv(url, parse_dates=['date'])
-
     def split_data(self, data):
         return data.iloc[:, :-self.n_forecast], data.iloc[:, -self.n_forecast:]
 
     def load_and_prep(self, urlpath):
-        data = self.load_nyt(urlpath)
-        data = self.prep_pipe.fit_transform(data)
-        self.geodf = data.groupby('geoid').agg('last')[['fips', 'geometry']]
+        data = pd.read_csv(urlpath, parse_dates=['date'])
+        return self.prep_pipe.fit_transform(data)
+
+    def create_spatial_data(self, data):
+        data = self.spatial_pipe.fit_transform(data.groupby('geoid').agg('last').drop(['date','cases','deaths'], axis=1).reset_index())
+        if self.scaler_dict is not None:
+            self.scaler_pipe = Pipeline([(k, TargetScaler(divisor=data.set_index('geoid')[v[0]].astype(float), multiplier=v[1])) for k,v in self.scaler_dict.items()])
         return data
 
-    def create_static_features(self, data):
-        return
-
-    def create_ts_feature(self, data, target, diff=True):
+    def create_ts(self, data, target, diff=True):
         data = data.pivot(index='geoid', columns='date', values=target).fillna(0)
         if diff: data = data.diff(axis=1)
+        if hasattr(self, 'scaler_pipe'): data = self.scaler_pipe.fit_transform(data)
         return data
 
     def run_inference(self, data, final_pipe, cols):
@@ -83,21 +88,22 @@ class COVPredict:
         yhat = final_pipe[:-1].inverse_transform(yhat)
         return X, y, yhat
 
-    def cv(self, k, urlpath):
+    def cv(self, k, urlpath, target):
         data = self.load_and_prep(urlpath)
-        data = self.create_ts_feature(data, self.target)
-        col_chunks = self.expandingsplit(data.columns, k)
+        spatial_data = self.create_spatial_data(data)
+        X = self.create_ts(data, target)
+        col_chunks = self.expandingsplit(X.columns, k)
         final_pipe = self.ts_pipe
-        return {f'fold_{ct}': self.run_inference(data, final_pipe, cols) for ct, cols in enumerate(col_chunks)}
+        return {f'fold_{ct}': self.run_inference(X, final_pipe, cols) for ct, cols in enumerate(col_chunks)}
 
-    def cv_plot_ts(self, results, idx):
+    def cv_plot_ts(self, results, idx, target):
         for k, v in results.items():
             plot_ts(v[0], idx=idx, c='steelblue', s=5, label='actual')
             plot_ts(v[2], idx=idx, c='indianred', s=20, label=k+' forecast')
 
         title_suffix = 'mean across obs'
         if isinstance(idx, str): title_suffix = idx
-        plt.title(f'actual and predicted {self.target} by cross-validation fold: {title_suffix}')
+        plt.title(f'actual and predicted {target} by cross-validation fold: {title_suffix}')
         plt.legend()
         plt.show()
         return
@@ -110,13 +116,14 @@ class COVPredict:
         boxplot_error(err)
         return
 
-    def final_test(self, urlpath):
-        data = self.load_and_prep(urlpath)
-        data = self.create_ts_feature(data, self.target)
+    def final_test(self, urlpath, target):
+        self.prep_data = self.load_and_prep(urlpath)
+        self.spatial_data = self.create_spatial_data(self.prep_data)
+        X = self.create_ts(self.prep_data, target)
         self.final_pipe = self.ts_pipe
-        self.final_X, self.final_y, self.final_yhat = self.run_inference(data, self.final_pipe, data.columns)
+        self.final_X, self.final_y, self.final_yhat = self.run_inference(X, self.final_pipe, X.columns)
 
-    def final_plots_and_error(self, idx, err_func):
+    def final_plots_and_error(self, idx, target, err_func):
         self.final_err = err_func(self.final_y, self.final_yhat).mean().mean()
 
         fig = plt.figure()
@@ -124,15 +131,16 @@ class COVPredict:
         label_suffix = 'mean across obs'
         if isinstance(idx, str): label_suffix = idx
         plot_ts(self.final_yhat, idx=idx, c='indianred', lw=3.5, label=f'forecast for {label_suffix}')
-        plt.title(f'actual and predicted {self.target}; err: {self.final_err:.4f}')
+        plt.title(f'actual and predicted {target}; err: {self.final_err:.4f}')
 
         fig = plt.figure()
-        heatmap(df=pd.concat([self.final_X, self.final_yhat], axis=1), target=self.target, sort_col=self.final_X.columns[-1], forecast_line=self.n_forecast)
+        heatmap(df=pd.concat([self.final_X, self.final_yhat], axis=1), target=target, sort_col=self.final_X.columns[-1], forecast_line=self.n_forecast)
         return
 
-    def get_forecast(self, urlpath):
-        data = self.load_and_prep(urlpath)
-        X = self.create_ts_feature(data, self.target)
+    def get_forecast(self, urlpath, target):
+        prep_data = self.load_and_prep(urlpath)
+        spatial_data = self.create_spatial_data(prep_data)
+        X = self.create_ts(prep_data, target)
         forecast_cols = pd.date_range(start=X.columns[-1] + datetime.timedelta(days=1), periods=self.n_forecast, freq='D')
         pipe = self.ts_pipe
         # empty df to set forecast dims
@@ -142,25 +150,25 @@ class COVPredict:
         yhat.columns = forecast_cols
         return X, yhat
 
-    def plot_forecast(self, X, yhat, idx):
+    def plot_forecast(self, X, yhat, idx, inverse_scale=False):
+        if inverse_scale:
+            X = self.scaler_pipe.inverse_transform(X)
+            yhat = self.scaler_pipe.inverse_transform(yhat)
         plot_ts(X, idx=idx, c='steelblue', label='actual')
         plot_ts(yhat, idx=idx, c='indianred', label='forecast')
         label_suffix = 'mean across obs'
         if isinstance(idx, str): label_suffix = idx
-        scale = 'count'
-        if 'per_capita' in self.prep_pipe.named_steps.keys(): scale = f"per {self.prep_pipe.get_params()['per_capita__multiplier']:.0f} people"
-        if 'per_capita_km' in self.prep_pipe.named_steps.keys(): scale += ' per km'
-        plt.title(f'actual {self.target} + {self.n_forecast} day forecast for {label_suffix} ({scale})')
+        plt.title(f'actual + {self.n_forecast} day forecast for {label_suffix}')
         plt.legend()
         plt.show()
 
-    def map_plot(self, col, title):
-        gpd.GeoDataFrame(pd.concat([self.geodf, col], axis=1, sort=True),
+    def map_plot(self, col, title, **plotkwargs):
+        gpd.GeoDataFrame(pd.concat([self.prep_data[['geometry']], col], axis=1, sort=True),
                          geometry='geometry',
-                         crs='EPSG:4326').plot(column=0, legend=True)
+                         crs='EPSG:4326').plot(column=0, legend=True, **plotkwargs)
         plt.title(title)
-        plt.xlim([-125, -60])
-        plt.ylim([15, 50])
+        plt.xlim([-127, -66])
+        plt.ylim([23, 50])
         plt.show()
 
     def save_obj(self, opath):
--- transformers/scaler.py
+++ transformers/scaler.py
@@ -1,18 +1,15 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class TargetScaler(BaseEstimator, TransformerMixin):
-    def __init__(self, target_cols, divisor_col, multiplier=100.0):
-        self.target_cols = target_cols
-        self.divisor_col = divisor_col
+    def __init__(self, divisor, multiplier=100.0):
+        self.divisor = divisor
         self.multiplier = multiplier
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        X.loc[:, self.target_cols] = (X.loc[:, self.target_cols] * self.multiplier).div(X[self.divisor_col].astype(float), axis=0)
-        return X
+        return (X * self.multiplier).div(self.divisor, axis=0)
 
     def inverse_transform(self, X):
-        X.loc[:, self.target_cols] = (X.loc[:, self.target_cols].multiply(X[self.divisor_col].astype(float), axis=0) / self.multiplier).round()
-        return X
+        return X.multiply(self.divisor) / self.multiplier
