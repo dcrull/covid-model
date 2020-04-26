3a289abc4b7c3275bcf9791cfa0c90356a87a53c
daynan@gmail.com <daynan@gmail.com>
2020-04-26 12:21:00 -0400

    refactor for non-NYT census data

--- plotting.py
+++ plotting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import pyplot,colors
 import pandas as pd
+import geopandas as gpd
 
 def trim_leading(data, thresh):
     idx = 0
@@ -40,3 +41,46 @@ def boxplot_error(errdata):
     errdata.columns = [i.date() for i in errdata.columns]
     sns.boxplot(x='variable', y='value', data=pd.melt(errdata))
     plt.show()
+
+def choro_plot(df, choro_col, title, **plotkwargs):
+    # just plots lower 48 states
+    df = df.loc[df['geometry'].notnull(), :]
+    gpd.GeoDataFrame(df, geometry='geometry', crs='EPSG:4326').plot(column=choro_col, legend=True, **plotkwargs)
+    plt.title(title)
+    plt.xlim([-127, -66])
+    plt.ylim([23, 50])
+    plt.show()
+
+def choro_cum_ct(spatial_data, obs_data, choro_col, title, multiplier=100000.0, **plotkwargs):
+    last_cum = obs_data.loc[obs_data['date']==obs_data['date'].max(), ['geoid','cases','deaths']].set_index('geoid')
+    df = pd.concat([spatial_data.set_index('geoid')[['geometry','POP']], last_cum], axis=1, sort=True)
+    df.loc[:, ['cases','deaths']] = (df.loc[: ,['cases', 'deaths']].astype(float) * multiplier).div(df['POP'].astype(float), axis=0)
+    choro_plot(df, choro_col, title, **plotkwargs)
+
+
+#NOTES:
+# - use 'inferno' for choropleth
+# use scheme="fisher_jenks" for total cases state/county
+
+# Plots and descriptive analytics
+# choropleth map of total cases/deaths state/county (scaled by pop)
+# choropleth map of growth in cases/deaths state/county (scaled by pop in last week)
+# TS of total cases/deaths state/county (mean)
+# TS of total cases/deaths state/count (top 10 most in last week; fastest growth in last week)
+# distribution of cases/deaths states/county
+# distribution of growth rate cases/deaths state/county
+
+## Predictions
+# Total cases (1,3,7, 30 days)
+# Top 10 states/county (1,3,7,30 days)
+
+# metrics @ diagnostics (states/counties cases/deaths)
+# Error 1 vs t-1; 3 vs t-3, 7 vs t-7, 30 vs t-30
+# top 10 best predictions states/counties (7 day)
+# top 10 worst predictions states/counties (7 day)
+
+# Maps
+# choropleth map of total forecast cases/deaths state/county (7 days)
+# choropleth of growth cases/deaths state/county (7 days)
+# choropleth of error cases/deaths state/county (7 days)
+# heatmap + forecast cases/deaths state/county
